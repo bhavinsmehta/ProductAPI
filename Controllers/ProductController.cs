0682b5c297c69d6d6bc6fa0fcaa8b20150cb05dc
bhavinsmehta <44048406+bhavinsmehta@users.noreply.github.com>
2019-05-18 10:50:34 +0800

    Intial Comit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProductAPI.Models;
+
+namespace ProductAPI.Controllers
+{
+    [Route("api/[controller]")] 
+    [ApiController]
+   public class ProductController:ControllerBase
+   {
+       // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetAll() => new List<Product>{
+        new Product {
+            ProductId = 1,
+            ProductName = "Id1",
+            ProductDescription = "Desc 1"
+        },
+        new Product {
+            ProductId = 2,
+            ProductName = "Id2",
+            ProductDescription = "Desc 2"
+        }
+    };
+
+
+   }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,9 @@
+namespace ProductAPI.Models
+{
+
+    public class Product{
+        public int ProductId {get;set;}
+        public string ProductName {get;set;}
+        public string ProductDescription {get;set;}
+    }
+}
